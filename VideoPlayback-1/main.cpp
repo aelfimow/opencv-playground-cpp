b7321a17b1c2334d97f9c7e58e845cdc83bf0bdd
Alexander Elfimow <my.free.mail@gmx.de>
2021-06-06 17:00:45 +0200

    VideoPlayback-1: added source file

---
+++ VideoPlayback-1/main.cpp
@@ -0,0 +1,39 @@
+#include <opencv2/opencv.hpp>
+#include <cstdlib>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+	if (argc < 2)
+	{
+		return EXIT_FAILURE;
+	}
+
+	std::string const filename { argv[1] };
+
+	cv::namedWindow("VideoPlayback-1", cv::WINDOW_AUTOSIZE);
+
+	cv::VideoCapture cap;
+	cap.open(filename);
+
+	bool finished = false;
+
+	while (not finished)
+	{
+		cv::Mat frame;
+
+		cap >> frame;
+
+		finished = frame.empty();
+
+		if (not finished)
+		{
+			cv::imshow("VideoPlayback-1", frame);
+			finished = (cv::waitKey(33) >= 0);
+		}
+	}
+
+	cv::destroyWindow("VideoPlayback-1");
+
+	return EXIT_SUCCESS;
+}
