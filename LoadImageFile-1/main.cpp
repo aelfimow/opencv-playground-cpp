2d548f81c96c07ed7904f170b1c4279ed61db9ac
Alexander Elfimow <my.free.mail@gmx.de>
2021-06-06 11:51:11 +0200

    LoadImageFile-1: added source file

---
+++ LoadImageFile-1/main.cpp
@@ -0,0 +1,26 @@
+#include <opencv2/opencv.hpp>
+#include <cstdlib>
+
+int main(int argc, char* argv[])
+{
+	if (argc < 2)
+	{
+		return EXIT_FAILURE;
+	}
+
+	cv::Mat img = cv::imread(argv[1], -1);
+
+	if (img.empty())
+	{
+		return EXIT_FAILURE;
+	}
+
+	cv::namedWindow("LoadImageFile-1", cv::WINDOW_AUTOSIZE);
+	cv::imshow("LoadImageFile-1", img);
+
+	cv::waitKey(0);
+
+	cv::destroyWindow("LoadImageFile-1");
+
+	return EXIT_SUCCESS;
+}
